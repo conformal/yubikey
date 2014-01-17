65ac3de5ed8f250baa58a3280ccf5351d241b6a0
David Hill <dhill@conformal.com>
2014-01-17 15:58:16 -0500

    [Yy]ubikey -> YubiKey

--- doc.go
+++ doc.go
@@ -1,13 +1,13 @@
 /*
-   Package yubikey implements the Yubico Yubikey OTP API, using 6-byte
+   Package yubikey implements the Yubico YubiKey OTP API, using 6-byte
    public identities and 16-byte secret keys.
 
-   Given a Yubikey private key and the generated OTP, this package
+   Given a YubiKey private key and the generated OTP, this package
    provides for validation of OTP tokens.
 
    A key is set up by passing the bytes into the NewKey function;
-   Yubikey secret keys are 32-bytes and hex-encoded. For example,
-   the Yubikey personalisation tool will provide a key like
+   YubiKey secret keys are 32-bytes and hex-encoded. For example,
+   the YubiKey personalisation tool will provide a key like
    "99cbcef30228f2539aa20358c46c0ad2".
 
    A typical OTP token looks something like
@@ -19,7 +19,7 @@
    This OTP can be validated and turned into a token using the Parse
    method. The NewOTP requires a string containing only the 32-byte
    token, while ParseOTPString will take the string directly from
-   the Yubikey and returns a UID and OTP.
+   the YubiKey and returns a UID and OTP.
 
    See examples/login/login.go for an example login authentication
    flow.
